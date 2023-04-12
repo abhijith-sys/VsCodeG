d04baa0ac2bc01a2a60fbe7271cbd81e3bf6a441
alenjoy7 <alenkalluvachel08@gmail.com>
2023-04-12 12:40:52 +0530

    html file added

--- tictac/dist/extension.js
+++ tictac/dist/extension.js
@@ -7,6 +7,18 @@
 
 module.exports = require("vscode");
 
+/***/ }),
+/* 2 */
+/***/ ((module) => {
+
+module.exports = require("fs");
+
+/***/ }),
+/* 3 */
+/***/ ((module) => {
+
+module.exports = require("path");
+
 /***/ })
 /******/ 	]);
 /************************************************************************/
@@ -45,26 +57,23 @@ exports.deactivate = exports.activate = void 0;
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = __webpack_require__(1);
+const fs = __webpack_require__(2);
+const path = __webpack_require__(3);
 const vscode_1 = __webpack_require__(1);
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+// /home/alenjoy/Desktop/vscodeExtension/VsCodeG/tictac/src/index.html
 function activate(context) {
     function createWebviewPanel(context) {
-        const panel = vscode_1.window.createWebviewPanel('helloWorld', 'Hello World', vscode_1.ViewColumn.One, {});
-        panel.webview.html = `
-		  <html>
-			<head>
-			  <style>
-				body {
-				  color: red;
-				}
-			  </style>
-			</head>
-			<body>
-			  <h1>Hello</h1>
-			</body>
-		  </html>
-		`;
+        const panel = vscode_1.window.createWebviewPanel("helloWorld", "Hello World", vscode_1.ViewColumn.One, {});
+        const htmlPath = path.join(context.extensionPath, "/src/index.html");
+        if (fs.existsSync(htmlPath)) {
+            const htmlContent = fs.readFileSync(htmlPath, "utf8");
+            panel.webview.html = htmlContent;
+        }
+        else {
+            console.log("HTML file not found");
+        }
     }
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
@@ -72,11 +81,11 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('tictac.helloWorld', () => {
+    let disposable = vscode.commands.registerCommand("tictac.helloWorld", () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
         createWebviewPanel(context);
-        vscode.window.showInformationMessage('!!!!!!!!!!!!!  new Game !');
+        vscode.window.showInformationMessage("!!!!!!!!!!!!!  new Game !");
     });
     context.subscriptions.push(disposable);
 }
--- tictac/src/extension.ts
+++ tictac/src/extension.ts
@@ -1,52 +1,47 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { window, ViewColumn, ExtensionContext } from 'vscode';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
+
+import { window, ViewColumn, ExtensionContext } from "vscode";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-
-
-
+// /home/alenjoy/Desktop/vscodeExtension/VsCodeG/tictac/src/index.html
 export function activate(context: vscode.ExtensionContext) {
-	function createWebviewPanel(context: ExtensionContext) {
-		const panel = window.createWebviewPanel(
-		  'helloWorld',
-		  'Hello World',
-		  ViewColumn.One,
-		  {}
-		);
-	  
-		panel.webview.html = `
-		  <html>
-			<head>
-			  <style>
-				body {
-				  color: red;
-				}
-			  </style>
-			</head>
-			<body>
-			  <h1>Hello</h1>
-			</body>
-		  </html>
-		`;
-	  }
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tictac" is now active!');
+  function createWebviewPanel(context: ExtensionContext) {
+    const panel = window.createWebviewPanel(
+      "helloWorld",
+      "Hello World",
+      ViewColumn.One,
+      {}
+    );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tictac.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+    const htmlPath = path.join(context.extensionPath, "/src/index.html");
 
-		createWebviewPanel(context);
-		vscode.window.showInformationMessage('!!!!!!!!!!!!!  new Game !');
-	});
+    if (fs.existsSync(htmlPath)) {
+      const htmlContent = fs.readFileSync(htmlPath, "utf8");
+      panel.webview.html = htmlContent;
+    } else {
+      console.log("HTML file not found");
+    }
+  }
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "tictac" is now active!');
 
-	context.subscriptions.push(disposable);
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand("tictac.helloWorld", () => {
+    // The code you place here will be executed every time your command is executed
+    // Display a message box to the user
+
+    createWebviewPanel(context);
+    vscode.window.showInformationMessage("!!!!!!!!!!!!!  new Game !");
+  });
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
