141ecf420eebd9931bc8cdaef4680cbc753c1b8e
alenjoy7 <alenkalluvachel08@gmail.com>
2023-04-12 15:14:25 +0530

    tic toc implemented

--- tictac/src/extension.ts
+++ tictac/src/extension.ts
@@ -12,19 +12,172 @@ export function activate(context: vscode.ExtensionContext) {
   function createWebviewPanel(context: ExtensionContext) {
     const panel = window.createWebviewPanel(
       "helloWorld",
-      "Hello World",
+      "tic tac",
       ViewColumn.One,
-      {}
+      {
+        enableScripts: true,
+      }
     );
 
-    const htmlPath = path.join(context.extensionPath, "/src/index.html");
+    const htmlPath = vscode.Uri.joinPath(
+      context.extensionUri,
+      "media",
+      "index.html"
+    );
+    const stylePath = vscode.Uri.joinPath(
+      context.extensionUri,
+      "media",
+      "style.css"
+    );
+    const scriptPath = vscode.Uri.joinPath(
+      context.extensionUri,
+      "media",
+      "main.js"
+    );
+    const htmlContent = panel.webview.asWebviewUri(htmlPath);
+    const styleContent = panel.webview.asWebviewUri(stylePath);
 
-    if (fs.existsSync(htmlPath)) {
-      const htmlContent = fs.readFileSync(htmlPath, "utf8");
-      panel.webview.html = htmlContent;
-    } else {
-      console.log("HTML file not found");
-    }
+    const mainScript = panel.webview.asWebviewUri(scriptPath);
+    // panel.webview.html = `<!DOCTYPE html>
+    //   <html lang="en">
+
+    //   <head>
+    //       <meta charset="UTF-8">
+    //       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    //       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //       <title>Document</title>
+    //       <link rel="stylesheet" href="${styleContent}">
+    //   </head>
+
+    //   <body>
+    //       <div id="intro-screen" class="center">
+    //           <h2>Choose your symbol:</h2>
+    //           <button type="button" id="choose-x" class="choose">X</button>
+    //           <button type="button" id="choose-o" class="choose">O</button>
+    //       </div>
+
+    //       <div id="enemy-screen" class="center hidden">
+    //           <h2>Play against:</h2>
+    //           <button type="button" id="choose-human" class="choose">Human</button>
+    //           <button type="button" id="choose-cpu" class="choose">CPU</button>
+    //       </div>
+
+    //       <div id="game-screen" class="center hidden">
+    //           <!-- Every cell has an id "cell" followed by cell row and cell column -->
+    //           <table class="center">
+    //               <tr>
+    //                   <td>
+    //                       <button type="button" class="cell" id="cell00"></button>
+    //                   </td>
+    //                   <td>
+    //                       <button type="button" class="cell" id="cell01"></button>
+    //                   </td>
+    //                   <td>
+    //                       <button type="button" class="cell" id="cell02"></button>
+    //                   </td>
+    //               </tr>
+    //               <tr>
+    //                   <td>
+    //                       <button type="button" class="cell" id="cell10"></button>
+    //                   </td>
+    //                   <td>
+    //                       <button type="button" class="cell" id="cell11"></button>
+    //                   </td>
+    //                   <td>
+    //                       <button type="button" class="cell" id="cell12"></button>
+    //                   </td>
+    //               </tr>
+    //               <tr>
+    //                   <td>
+    //                       <button type="button" class="cell" id="cell20"></button>
+    //                   </td>
+    //                   <td>
+    //                       <button type="button" class="cell" id="cell21"></button>
+    //                   </td>
+    //                   <td>
+    //                       <button type="button" class="cell" id="cell22"></button>
+    //                   </td>
+    //               </tr>
+    //           </table>
+    //           <button type="button" id="restart">Restart</button>
+    //           <div>
+    //   </body>
+    //   <script src=${mainScript}></script>
+    //   </html>`;
+    panel.webview.html = `<!DOCTYPE html>
+   
+    <head>
+        <meta name="viewport" content=
+            "width=device-width, initial-scale=1.0">
+       
+        <link rel="stylesheet" href="${styleContent}">
+       
+        <script src=${mainScript}></script>
+    </head>
+       
+    <body>
+        <div id="main">
+            <h1>TIC TAC TOE</h1>
+       
+            <!-- Game Instructions -->
+            <p id="ins">Game starts by just Tap on 
+                box<br><br>First Player starts as 
+                <b>Player X</b><br>And<br>Second 
+                Player as <b>Player 0</b>
+            </p>
+     
+      
+       
+       
+            <br><br>
+            <!-- 3*3 grid of Boxes -->
+            <input type="text" id="b1" onclick=
+                "myfunc_3(); myfunc();" readonly>
+       
+            <input type="text" id="b2" onclick=
+                "myfunc_4(); myfunc();" readonly>
+       
+            <input type="text" id="b3" onclick=
+                "myfunc_5(); myfunc();" readonly>
+            <br><br>
+       
+            <input type="text" id="b4" onclick=
+                "myfunc_6(); myfunc();" readonly>
+                   
+            <input type="text" id="b5" onclick=
+                "myfunc_7(); myfunc();" readonly>
+       
+            <input type="text" id="b6" onclick=
+                "myfunc_8(); myfunc();" readonly>
+            <br><br>
+       
+            <input type="text" id="b7" onclick=
+                "myfunc_9(); myfunc();" readonly>
+       
+            <input type="text" id="b8" onclick=
+                "myfunc_10();myfunc();" readonly>
+       
+            <input type="text" id="b9" onclick=
+                "myfunc_11();myfunc();" readonly>
+       
+            <!-- Grid end here  -->
+            <br><br><br>
+            <!-- Button to reset game -->
+            <button id="but" onclick="myfunc_2()">
+                RESET
+            </button>
+       
+            <br><br>
+            <!-- Space to show player turn -->
+            <p id="print"></p>
+     
+      
+       
+       
+        </div>
+    </body>
+       
+    </html>`;
   }
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
