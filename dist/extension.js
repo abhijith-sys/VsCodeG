5416f06d2b448e3d4782bcd0acfc088b81d3b70d
alenjoy7 <alenkalluvachel08@gmail.com>
2023-04-14 14:57:05 +0530

    react added and tictactoe click event added

--- dist/extension.js
+++ dist/extension.js
@@ -62,79 +62,17 @@ function activate(context) {
         const styleContent = panel.webview.asWebviewUri(stylePath);
         const mainScript = panel.webview.asWebviewUri(scriptPath);
         const threejsScript = panel.webview.asWebviewUri(threejsScriptPath);
-        // panel.webview.html = `<!DOCTYPE html>
-        //   <html lang="en">
-        //   <head>
-        //       <meta charset="UTF-8">
-        //       <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        //       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        //       <title>Document</title>
-        //       <link rel="stylesheet" href="${styleContent}">
-        //   </head>
-        //   <body>
-        //       <div id="intro-screen" class="center">
-        //           <h2>Choose your symbol:</h2>
-        //           <button type="button" id="choose-x" class="choose">X</button>
-        //           <button type="button" id="choose-o" class="choose">O</button>
-        //       </div>
-        //       <div id="enemy-screen" class="center hidden">
-        //           <h2>Play against:</h2>
-        //           <button type="button" id="choose-human" class="choose">Human</button>
-        //           <button type="button" id="choose-cpu" class="choose">CPU</button>
-        //       </div>
-        //       <div id="game-screen" class="center hidden">
-        //           <!-- Every cell has an id "cell" followed by cell row and cell column -->
-        //           <table class="center">
-        //               <tr>
-        //                   <td>
-        //                       <button type="button" class="cell" id="cell00"></button>
-        //                   </td>
-        //                   <td>
-        //                       <button type="button" class="cell" id="cell01"></button>
-        //                   </td>
-        //                   <td>
-        //                       <button type="button" class="cell" id="cell02"></button>
-        //                   </td>
-        //               </tr>
-        //               <tr>
-        //                   <td>
-        //                       <button type="button" class="cell" id="cell10"></button>
-        //                   </td>
-        //                   <td>
-        //                       <button type="button" class="cell" id="cell11"></button>
-        //                   </td>
-        //                   <td>
-        //                       <button type="button" class="cell" id="cell12"></button>
-        //                   </td>
-        //               </tr>
-        //               <tr>
-        //                   <td>
-        //                       <button type="button" class="cell" id="cell20"></button>
-        //                   </td>
-        //                   <td>
-        //                       <button type="button" class="cell" id="cell21"></button>
-        //                   </td>
-        //                   <td>
-        //                       <button type="button" class="cell" id="cell22"></button>
-        //                   </td>
-        //               </tr>
-        //           </table>
-        //           <button type="button" id="restart">Restart</button>
-        //           <div>
-        //   </body>
-        //   <script src=${mainScript}></script>
-        //   </html>`;
-        const htmlTemplate = `<!DOCTYPE html>
-   
+        const htmlTemplate = `
+    <!DOCTYPE html>
+
     <head>
-        <meta name="viewport" content=
-            "width=device-width, initial-scale=1.0">
-       
-        <link rel="stylesheet" href="${styleContent}">
-       
-        <script async src="https://unpkg.com/es-module-shims@1.6.3/dist/es-module-shims.js"></script>
-        
-        <script type="importmap">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+      <link rel="stylesheet" href="${styleContent}">
+    
+      <script async src="https://unpkg.com/es-module-shims@1.6.3/dist/es-module-shims.js"></script>
+    
+      <script type="importmap">
         {
           "imports": {
             "three": "https://unpkg.com/three@0.151.3/build/three.module.js",
@@ -142,73 +80,78 @@ function activate(context) {
           }
         }
         </script>
-        <script type="module" src=${threejsScript}></script>
-        <script  src=${mainScript}></script>
+      <script type="module" src=${threejsScript}></script>
+      <script src=${mainScript}></script>
     </head>
-       
+    
     <body>
-        <div id="main">
-            <h1>TIC TAC TOE</h1>
+      <div id="main">
+        <h1>TIC TAC TOE</h1>
         <div id="canvas"></div>
-            <!-- Game Instructions -->
-            <p id="ins">Game starts by just Tap on 
-                box<br><br>First Player starts as 
-                <b>Player X</b><br>And<br>Second 
-                Player as <b>Player 0</b>
-            </p>
-     
-      
-       
-       
-            <br><br>
-            <!-- 3*3 grid of Boxes -->
-            <input type="text" id="b1" onclick=
-                "myfunc_3(); myfunc();" readonly>
-       
-            <input type="text" id="b2" onclick=
-                "myfunc_4(); myfunc();" readonly>
-       
-            <input type="text" id="b3" onclick=
-                "myfunc_5(); myfunc();" readonly>
-            <br><br>
-       
-            <input type="text" id="b4" onclick=
-                "myfunc_6(); myfunc();" readonly>
-                   
-            <input type="text" id="b5" onclick=
-                "myfunc_7(); myfunc();" readonly>
-       
-            <input type="text" id="b6" onclick=
-                "myfunc_8(); myfunc();" readonly>
-            <br><br>
-       
-            <input type="text" id="b7" onclick=
-                "myfunc_9(); myfunc();" readonly>
-       
-            <input type="text" id="b8" onclick=
-                "myfunc_10();myfunc();" readonly>
-       
-            <input type="text" id="b9" onclick=
-                "myfunc_11();myfunc();" readonly>
-       
-            <!-- Grid end here  -->
-            <br><br><br>
-            <!-- Button to reset game -->
-            <button id="but" onclick="myfunc_2()">
-                RESET
-            </button>
-       
-            <br><br>
-            <!-- Space to show player turn -->
-            <p id="print"></p>
-     
-      
-       
-       
-        </div>
+        <!-- Game Instructions -->
+        <p id="ins">Game starts by just Tap on
+          box<br><br>First Player starts as
+          <b>Player X</b><br>And<br>Second
+          Player as <b>Player 0</b>
+        </p>
+        <br><br>
+        <!-- 3*3 grid of Boxes -->
+        <input type="text" id="b1" onclick="myfunc_3(); myfunc();" readonly>
+    
+        <input type="text" id="b2" onclick="myfunc_4(); myfunc();" readonly>
+    
+        <input type="text" id="b3" onclick="myfunc_5(); myfunc();" readonly>
+        <br><br>
+    
+        <input type="text" id="b4" onclick="myfunc_6(); myfunc();" readonly>
+    
+        <input type="text" id="b5" onclick="myfunc_7(); myfunc();" readonly>
+    
+        <input type="text" id="b6" onclick="myfunc_8(); myfunc();" readonly>
+        <br><br>
+    
+        <input type="text" id="b7" onclick="myfunc_9(); myfunc();" readonly>
+    
+        <input type="text" id="b8" onclick="myfunc_10();myfunc();" readonly>
+    
+        <input type="text" id="b9" onclick="myfunc_11();myfunc();" readonly>
+    
+        <!-- Grid end here  -->
+        <br><br><br>
+        <!-- Button to reset game -->
+        <button id="but" onclick="myfunc_2()">
+          RESET
+        </button>
+    
+        <br><br>
+        <!-- Space to show player turn -->
+        <p id="print"></p>
+      </div>
     </body>
-       
-    </!DOCTYPE>`;
+    
+    </html>
+`;
+        const reactPanelHtml = `
+    <!DOCTYPE html>
+    <html>
+    
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="Content-Security-Policy"
+        content="default-src 'none'; script-src 'self' vscode-resource:; style-src 'self' vscode-resource: 'unsafe-inline'; img-src vscode-resource: data:;">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <base href="${panel.webview.asWebviewUri(vscode.Uri.file(context.asAbsolutePath(" media/tic/dist/")))}" />
+      <title>My React Panel</title>
+      <link rel="stylesheet" href="assets/index.css" />
+    </head>
+    
+    <body>
+      <div id="root"></div>
+      <script src="assets/index.js"></script>
+    </body>
+    
+    </html>
+`;
         panel.webview.html = htmlTemplate;
     }
     // Use the console to output diagnostic information (console.log) and errors (console.error)
--- media/threejs.js
+++ media/threejs.js
@@ -1,6 +1,6 @@
-const myCanvas = document.querySelector('#canvas');
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+const myCanvas = document.querySelector("#canvas");
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
@@ -10,7 +10,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 myCanvas.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -36,18 +36,45 @@ cube1.position.set(5, 0);
 cube2.position.set(-5, 0);
 cube3.position.set(0, 5);
 cube4.position.set(0, -5);
+
+const boxGeo = new THREE.BoxGeometry(9.5, 9.5, 1);
+const boxMat = new THREE.MeshBasicMaterial({
+  opacity: 0.00001,
+  transparent: true,
+});
+const innerCube1 = new THREE.Mesh(boxGeo, boxMat);
+const innerCube2 = new THREE.Mesh(boxGeo, boxMat);
+const innerCube3 = new THREE.Mesh(boxGeo, boxMat);
+const innerCube4 = new THREE.Mesh(boxGeo, boxMat);
+const innerCube5 = new THREE.Mesh(boxGeo, boxMat);
+const innerCube6 = new THREE.Mesh(boxGeo, boxMat);
+const innerCube7 = new THREE.Mesh(boxGeo, boxMat);
+const innerCube8 = new THREE.Mesh(boxGeo, boxMat);
+const innerCube9 = new THREE.Mesh(boxGeo, boxMat);
+
+scene.add(innerCube1);
+scene.add(innerCube2);
+scene.add(innerCube3);
+scene.add(innerCube4);
+scene.add(innerCube5);
+scene.add(innerCube6);
+scene.add(innerCube7);
+scene.add(innerCube8);
+scene.add(innerCube9);
+
+innerCube2.position.set(10, 10, 0);
+innerCube3.position.set(10, 0, 0);
+innerCube4.position.set(0, 10, 0);
+innerCube5.position.set(0, -10, 0);
+innerCube6.position.set(-10, -10, 0);
+innerCube7.position.set(10, -10, 0);
+innerCube8.position.set(-10, 0, 0);
+innerCube9.position.set(-10, 10, 0);
+
 const sphereGeometry = new THREE.SphereGeometry(3, 32, 16);
 const sphereMaterial = new THREE.MeshStandardMaterial({
   color: 0xffff00,
 });
-const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-scene.add(sphere);
-
-const boxGeometry = new THREE.BoxGeometry(5, 5, 5);
-const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x2323c9 });
-const cube = new THREE.Mesh(boxGeometry, boxMaterial);
-scene.add(cube);
-cube.position.set(10, 0);
 
 const spotLight = new THREE.SpotLight(0xffffff);
 const spotLight1 = new THREE.SpotLight(0xffffff);
@@ -65,9 +92,60 @@ scene.add(spotLight);
 scene.add(spotLight1);
 
 const spotLightHelper = new THREE.SpotLightHelper(spotLight);
-//   scene.add(spotLightHelper);
 camera.position.z = 25;
 camera.position.y = 3;
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+let clickCount = 0;
+
+let gamestate = new Array(9).fill(0);
+
+const onClick = (event) => {
+  // Calculate the mouse position in normalized device coordinates
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  // Cast a ray from the camera to the clicked position
+  raycaster.setFromCamera(mouse, camera);
+
+  // Check if the ray intersects with any of the cubes
+  const intersects = raycaster.intersectObjects([
+    innerCube1,
+    innerCube2,
+    innerCube3,
+    innerCube4,
+    innerCube5,
+    innerCube6,
+    innerCube7,
+    innerCube8,
+    innerCube9,
+  ]);
+  if (intersects.length > 0) {
+    // Change the color of the clicked cube
+    const cube = intersects[0].object;
+
+    console.log(cube.position);
+    console.log(clickCount);
+    if (clickCount % 2 == 0) {
+      const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+      sphere.position.copy(cube.position);
+      scene.add(sphere);
+    } else {
+      const cube = intersects[0].object;
+      const boxGeometry = new THREE.BoxGeometry(5, 5, 5);
+      const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x2323c9 });
+      const cube1 = new THREE.Mesh(boxGeometry, boxMaterial);
+      cube1.position.copy(cube.position);
+      scene.add(cube1);
+    }
+    clickCount = clickCount + 1;
+    cube.material.color.setHex(Math.random() * 0xffffff);
+  }
+};
+
+window.addEventListener("click", onClick);
+
 function animate() {
   requestAnimationFrame(animate);
   controls.update();
